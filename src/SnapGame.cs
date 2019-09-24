f55a9535b39771ed660c3d1bb2b2fc2c4a8115a7
alexson <101221527@students.swinburne.edu.my>
2019-09-24 13:12:15 +0800

    Change Display Font

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -42,8 +42,8 @@ namespace CardGames
 			if (top != null)
 			{
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.White, "GameFont", 0, 20);
-				SwinGame.DrawText ("Player 1 score:" + myGame.Score (0), Color.White, "GameFont", 0, 40);
-				SwinGame.DrawText ("Player 2 score:" + myGame.Score (1), Color.White, "GameFont", 0, 70);
+				SwinGame.DrawText ("Player 1 score:" + myGame.Score (0), Color.White, "GameFont", 0, 50);
+				SwinGame.DrawText ("Player 2 score:" + myGame.Score (1), Color.White, "GameFont", 0, 75);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 
